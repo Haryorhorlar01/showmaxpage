84d15f2b028cc048ed280bdb35820e7ba1c7be72
Haryorhorlar01 <ayoolagbengagodwin@gmail.com>
2023-04-21 17:48:26 +0200

    modified script for accordion section

--- JS/main.js
+++ JS/main.js
@@ -1,37 +1,35 @@
 const accordionContent = document.querySelectorAll('.accordion-content');
 
-Array.from(accordionContent).forEach((item, index) => {
-    let header = item.querySelector('header');
-    header.addEventListener('click', () => {
-        item.classList.toggle("open");
+accordionContent.forEach((item, index) => {
+    const header = item.querySelector('header');
 
-        let description = item.querySelector(".description");
+    header.addEventListener('click', () => {
+        item.classList.toggle('open');
+
+        let description = document.querySelector('.description');
 
         if (item.classList.contains("open")) {
             description.style.height = `${description.scrollHeight}px`;
-            item.querySelector("i").classList.replace("fa-plus", "fa-minus");
+            item.querySelector("i").classList.replace('fa-plus', 'fa-minus');
         } else {
             description.style.height = "0px";
-            item.querySelector('i').classList.replace("fa-minus", "fa-plus");
+            item.querySelector("i").classList.replace('fa-minus', 'fa-plus');
         }
 
         removeOpen(index);
-
-        console.log(description);
+        
     })
-
-})
-
+    
+});
 
 function removeOpen(index1) {
     accordionContent.forEach((item2, index2) => {
-        if (index1 != index2) {
+        if (index1 !== index2) {
             item2.classList.remove("open");
 
-            let des = item2.querySelector(".description");
-            des.style.height = "0px";
-            item2.querySelector("i").classList.replace("fa-minus", "fa-plus");
+            let descriptn = item2.querySelector(".description");
+            descriptn.style.height = "0px";
+            item2.querySelector('i').classList.replace('fa-minus', 'fa-plus');
         }
-    })
+    });
 }
-console.log(header);
